3f47bd7929381bce141e2bedec0f1460880dc251
DipakBist08 <mbdds54@gmail.com>
2025-08-17 02:40:53 +0545

    Launching a browser

---
+++ Launching_browser.py
@@ -0,0 +1,8 @@
+from playwright.sync_api import sync_playwright
+#playwright=sync_playwright().start()or
+with sync_playwright() as p:
+    browser=p.chromium.launch(headless=False,slow_mo=800)
+    #create a new page
+    page =browser.new_page()
+    page.goto("https://playwright.dev/python")
+    browser.close()
