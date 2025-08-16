7a9a0e3d4bdcbe29591793948937effb726639d4
DipakBist08 <mbdds54@gmail.com>
2025-08-17 03:08:39 +0545

    updated comment

--- Launching_browser.py
+++ Launching_browser.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import sync_playwright
 #playwright=sync_playwright().start()or
 with sync_playwright() as p:
-    browser=p.chromium.launch(headless=False,slow_mo=800)
+    browser=p.chromium.launch(headless=False,slow_mo=800) #slow_mo is used to see how you are interacting visually
     #create a new page
     page =browser.new_page()
     page.goto("https://playwright.dev/python") #playwright website
