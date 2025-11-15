2eccff746904dc6a5197390672fef5adb0c246c2
DipakBist08 <mbdds54@gmail.com>
2025-11-15 18:18:41 +0545

    Download Files

---
+++ Download_Files.py
@@ -0,0 +1,18 @@
+from playwright.sync_api import sync_playwright
+import time
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False,slow_mo=500)
+    page = browser.new_page()
+    page.goto("https://pixabay.com/photos/nature-forest-landscape-at-night-3194001/")
+
+    Image_download_dropdown = page.get_by_role("button",name="Download")
+    Image_download_dropdown.click()
+
+    Download_Options = page.locator("div.downloadItem--xBKyT").last
+    Download_Options.click()
+    Download_Btn = page.locator('div.buttons--cqw3Y > a:has-text("Download")')
+    Download_Btn.click()
+
+    time.sleep(10)
+
+    browser.close()
