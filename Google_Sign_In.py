bf2edac2ceb4a7d585edc68cf33585a15873090b
DipakBist08 <mbdds54@gmail.com>
2025-12-08 21:55:33 +0545

    Google Sign In Automation

---
+++ Google_Sign_In.py
@@ -0,0 +1,23 @@
+import  os
+from dotenv import load_dotenv
+from playwright.sync_api import sync_playwright
+load_dotenv()
+
+email = os.getenv('GOOGLE_EMAIL')
+password = os.getenv('GOOGLE_PASSWORD')
+print(f"EMAIL Loaded: {email}")
+print(f"Password loaded: {password}")
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False,slow_mo=500)
+    page = browser.new_page()
+    page.goto("https://accounts.google.com")
+    email_phone = page.get_by_label("Email or phone")
+    email_phone.clear()
+    email_phone.fill(email)
+    Next_button = page.get_by_role("button",name="Next")
+    Next_button.click()
+    Enter_password = page.get_by_label("Enter your password")
+    Enter_password.clear()
+    Enter_password.fill(password)
+    Next_button.click()
+    browser.close()
